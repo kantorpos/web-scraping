f512d202ba7bc89210379b67512773b61e2ece09
kantorpos <dennydotsaputro@gmail.com>
2021-06-17 21:01:45 +0700

    Web_Scrapping Tutorial Done

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('base.html')
 
 
 @app.route('/detik-populer')
@@ -16,7 +16,14 @@ def detik_populer():
     soup = BeautifulSoup(html_doc.text, "html.parser")
     tag_terpopuler = soup.find(attrs={'class': 'grid-row list-content'})
     images = tag_terpopuler.findAll(attrs={'class': 'media__link'})
-    return render_template('index.html', images=images)
+    return render_template('detik-scraper.html', images=images)
+
+
+@app.route('/idr-rates')
+def idr_rates():
+    source = requests.get('https://floatrates.com/daily/idr.json')
+    json_data = source.json()
+    return render_template('idr-rates.html', datas=json_data.values())
 
 
 if __name__ == '__main__':
